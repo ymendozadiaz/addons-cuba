31a2c4f18e41b716d2118d4b67c10667e9d77d00
ymendozadiaz <118143127+ymendozadiaz@users.noreply.github.com>
2022-11-22 15:17:37 +0100

    button full
    button full

--- om_Hospital/models/patient.py
+++ om_Hospital/models/patient.py
@@ -8,13 +8,26 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Patient"
 
-    name = fields.Char(string='Name', required=True)
-    age = fields.Integer(string='Age')
+    name = fields.Char(string='Name', required=True, tracking=True)
+    age = fields.Integer(string='Age', tracking=True)
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'Other')
-    ], required=True, default='male')
+    ], required=True, default='male', tracking=True)
     note = fields.Text(string='Description')
-    state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'), ('done', 'Done'), 
-                                ('cancel', 'Cancelled')], default='draft', string="Status")
+    state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'), ('done', 'Done')
+                                , ('cancel', 'Cancelled')], default='draft', string="Status")
+
+    def action_confirm(self):
+        self.state = 'confirm'
+
+    def action_done(self):
+        self.state = 'done'
+    
+    def action_draft(self):
+        self.state = 'draft'
+
+    def action_cancel(self):
+        self.state = 'cancel'
+        
