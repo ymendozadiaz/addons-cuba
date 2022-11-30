f411861c188e79989a1d3619fe4ee15c40e8bc97
Yosbel Mendoza Díaz <118143127+ymendozadiaz@users.noreply.github.com>
2022-11-30 10:57:44 -0500

    día 12

---
+++ dia_12/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Hospital Management',
+    'version' : '16.0',
+    'summary': 'Hospital Management Software',
+    'sequence': -100,
+    'description': """Hospital Management Software""",
+    'category': 'Productivity',
+    'website': '',
+    'depends' : [
+        'sale',
+        'mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'views/patient.xml',
+        'views/kids_view.xml',
+        'views/patient_gender_view.xml',
+        'views/appointment_view.xml',
+        'views/sale.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ dia_12/models/patient.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Hospital Patient"
+
+    name = fields.Char(string='Name', required=True, tracking=True)
+    reference = fields.Char(string='Order Reference', required=True, copy=False, readonly=True
+                            , default=lambda self:_('New'))
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ], required=True, default='male', tracking=True)
+    note = fields.Text(string='Description')
+    state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'), ('done', 'Done')
+                                 , ('cancel', 'Cancelled')], default='draft', string="Status", tracking=True)
+    responsible_id = fields.Many2one(comodel_name='res.partner', string="Responsible")
+    
+    appointment_count = fields.Integer(string='Appointment Count', compute='_compute_appointment_count')
+
+    def _compute_appointment_count(self):
+        appointment_count = self.env['hospital.appointment'].search_count([('patient_id', '=', self.id)])
+        self.appointment_count = appointment_count
+
+    def action_confirm(self):
+        self.state = 'confirm'
+
+    def action_done(self):
+        self.state = 'done'
+    
+    def action_draft(self):
+        self.state = 'draft'
+
+    def action_cancel(self):
+        self.state = 'cancel'
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('note'):
+            vals['note'] = "New Patient"
+        res = super(HospitalPatient, self).create(vals)
+        return res
+    
+    @api.model
+    def create(self, vals):
+        if not vals.get('note'):
+            vals['note'] = "New Patient"
+        if vals.get('reference', _('New')) == _('New'):
+            vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.patient') or _('New')
+        res = super(HospitalPatient, self).create(vals)
+        return res
