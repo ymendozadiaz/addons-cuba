a17263c5be1e799820ed55c018f1ee09dd396c59
Yosbel Mendoza Díaz <118143127+ymendozadiaz@users.noreply.github.com>
2022-12-03 10:33:39 -0500

    dia 15

---
+++ dia_15/wizard/create_appointment.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+
+class CreateAppointmentWizard(models.TransientModel):
+    _name = "create.appointment.wizard"
+    _description = "Create Appointment Wizard"
+
+    date_appointment = fields.Date(string='Date', required=True)
+    patient_id = fields.Many2one('hospital.patient', string='Patient', required=True)
+    
+    def action_create_appointment(self):
+        vals = {
+            'patient_id': self.patient_id.id,
+            'date_appointment': self.date_appointment
+        }
+        self.env['hospital.appointment'].create(vals)
