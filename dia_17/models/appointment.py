13d263e170281f02fb5f0f090ffd9faa0babc35b
Yosbel Mendoza Díaz <118143127+ymendozadiaz@users.noreply.github.com>
2022-12-05 18:47:36 -0500

    dia 17

---
+++ dia_17/models/appointment.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+
+class HospitalAppointment(models.Model):
+    _name = "hospital.appointment"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Hospital Appointment"
+
+    name = fields.Char(string='Order Reference', required=True, copy=False, readonly=True, default=lambda self: _('New'))
+
+    patient_id = fields.Many2one('hospital.patient', string='Patient', required=True)
+
+    age = fields.Integer(string='Age', related="patient_id.age", tracking=True)
+
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ], string="Gender")
+
+    state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'), ('done', 'Done')
+                                 , ('cancel', 'Cancelled')], default='draft', string="Status", tracking=True)
+    
+    note = fields.Text(string='Description')
+    
+    date_appointment = fields.Date(string="Date")
+    date_checkup = fields.Datetime(string="Check Up Time")
+
+    def action_confirm(self):
+        self.state = 'confirm'
+
+    def action_done(self):
+        self.state = 'done'
+    
+    def action_draft(self):
+        self.state = 'draft'
+
+    def action_cancel(self): 
+        self.state = 'cancel'
+   
+    @api.model
+    def create(self, vals):
+        if not vals.get('note'):
+            vals['note'] = "New Patient"
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('hospital.appointment') or _('New')
+        res = super(HospitalAppointment, self).create(vals)
+        return res
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        if self.patient_id:
+            if self.patient_id.gender:
+                self.gender = self.patient_id.gender
+            if self.patient_id.note:
+                self.note = self.patient_id.note
+                
+        else:
+            self.gender = ''
+            self.note = ''
+
+
---
+++ dia_17/models/doctor.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+
+class HospitalDoctor(models.Model):
+    _name = "hospital.doctor"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Hospital Doctor"
+
+    doctor_name = fields.Char(string='Name', required=True, tracking=True)
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ], required=True, default='male', tracking=True)
+    note = fields.Text(string='Description')    
+    image = fields.Binary(string="Patient Image")
