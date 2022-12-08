495c4fb71639855342e4284ac2c60d8756c10a0f
Yosbel Mendoza Díaz <118143127+ymendozadiaz@users.noreply.github.com>
2022-12-08 12:24:40 -0500

    dia 20

---
+++ dia_20/models/patient.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Hospital Patient"
+    _order = "id desc"
+
+@api.model
+def default_get(self, fields):
+    res = super(HospitalPatient, self).default_get(fields)
+    res['note'] = 'New Patient Created'
+    return res
+    
+    
+    name = fields.Char(string='Name', required=True, tracking=True)
+    reference = fields.Char(string='Order Reference', required=True, copy=False, readonly=True
+                            , default=lambda self:_('New'))
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ], required=True, default='male', tracking=True)
+    note = fields.Text(string='Description')
+    state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'), ('done', 'Done')
+                                 , ('cancel', 'Cancelled')], default='draft', string="Status", tracking=True)
+    responsible_id = fields.Many2one(comodel_name='res.partner', string="Responsible")
+    appointment_count = fields.Integer(string='Appointment Count', compute='_compute_appointment_count')
+    image = fields.Binary(string="Patient Imagen")
+    appointment_ids = fields.One2many('hospital.appointment', 'patient_id', string="Appointment")
+    
+    
+    def _compute_appointment_count(self):
+        for rec in self:
+            appointment_count = self.env['hospital.appointment'].search_count([('patient_id', '=', rec.id)])
+            rec.appointment_count = appointment_count
+
+    def action_confirm(self):
+        for rec in self:
+            rec.state = 'confirm'
+
+    def action_done(self):
+        for rec in self:
+            rec.state = 'done'
+    
+    def action_draft(self):
+        for rec in self:
+            rec.state = 'draft'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'cancel'
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('note'):
+            vals['note'] = "New Patient"
+        if vals.get('reference', _('New')) == _('New'):
+            vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.patient') or _('New')
+        res = super(HospitalPatient, self).create(vals)
+        return res
+    
+    @api.constrains('name')
+    def check_name(self):
+        for rec in self:
+            patients = self.env['hospital.patient'].search([('name', '=', 'rec.name'), ('id', '!=', rec.id)])
+            if patients:
+                raise ValidationError(_("Name %s Already Exists" % rec.name))
+            
+    @api.constrains('agee')
+    def check_age(self):
+        for rec in self:
+            if rec.age == 0:
+                raise ValidationError(_("Age Cannot be Zero...!"))
